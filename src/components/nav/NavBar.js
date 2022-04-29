a20439c8d52e30ab27773f465482ac50ca31b1e5
LeeRJennings <leerjennings@outlook.com>
2022-04-29 14:51:40 -0500

    a little navbar styling

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 export const NavBar = ({clearUser}) => {
     return (
         <nav>
             <ul>
-                <li>
-                    <Link to="/">All Games</Link>
-                </li>
-                <li>
-                    <Link to="/myGames">My Games</Link>
-                </li>
-                <li>
-                    <Link to="/" onClick={() => clearUser()}>Logout</Link>
-                </li>
+                <div className="nav-items">
+                    <li>
+                        <Link to="/">All Games</Link>
+                    </li>
+                    <li>
+                        <Link to="/myGames">My Games</Link>
+                    </li>
+                    <div className="logout">
+                        <li>
+                            <Link to="/" onClick={() => clearUser()}>Logout</Link>
+                        </li>
+                    </div>
+                </div>
             </ul>
         </nav>
     )
