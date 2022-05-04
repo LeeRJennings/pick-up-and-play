4b0dd250ea232cc276370391725cb388a516551b
LeeRJennings <leerjennings@outlook.com>
2022-05-04 13:11:34 -0500

    previous games have their own views

--- src/helpers/dateFormatter.js
+++ src/helpers/dateFormatter.js
@@ -6,3 +6,43 @@ export const dateFormatter = (string) => {
     const formattedDate = month + "/" + day + "/" + year 
     return formattedDate 
 }
+
+export const upcomingDateFormatter = (string) => {
+    const date = new Date(string) 
+    let day = date.getUTCDate() 
+    let month = date.getUTCMonth() +1
+    let year = date.getUTCFullYear()
+    if (parseInt(day) < 10 && parseInt(month) < 10) {
+        const formattedDate = `${year}-0${month}-0${day}`  
+        return formattedDate 
+    } else if (parseInt(day) < 10) {
+        const formattedDate = `${year}-${month}-0${day}`  
+        return formattedDate
+    } else if (parseInt(month) < 10) {
+        const formattedDate = `${year}-0${month}-${day}`  
+        return formattedDate 
+    } else {
+        const formattedDate = `${year}-${month}-${day}`  
+        return formattedDate
+    }
+}
+
+export const previousDateFormatter = (string) => {
+    const date = new Date(string) 
+    let day = date.getUTCDate() -1
+    let month = date.getUTCMonth() +1
+    let year = date.getUTCFullYear()
+    if (parseInt(day) < 10 && parseInt(month) < 10) {
+        const formattedDate = `${year}-0${month}-0${day}`  
+        return formattedDate 
+    } else if (parseInt(day) < 10) {
+        const formattedDate = `${year}-${month}-0${day}`  
+        return formattedDate
+    } else if (parseInt(month) < 10) {
+        const formattedDate = `${year}-0${month}-${day}`  
+        return formattedDate 
+    } else {
+        const formattedDate = `${year}-${month}-${day}`  
+        return formattedDate
+    }
+}
